f0e31fa1bd04162ecb0be64b623caed2c81c9a35
francescobaiocchi <frappabaio@libero.it>
2022-01-22 04:02:53 +0100

    Primo commit del progetto. C'è la presenza di 3 importanti Fix me, per i due metodi collect_evidence e find_leaf della classe Junction Tree e per il metodo joint_distribuition della classe Belief Network.

---
+++ Inference.py
@@ -0,0 +1,198 @@
+import math
+import random
+
+import numpy as np
+import python_algorithms.basic.union_find as uf
+
+
+class BeliefNetwork:
+    def __init__(self, nodes, parents, cpt):
+        self.nodes = nodes
+        # Parents è un dizionario che associa a una variabile (nodo) i suoi parents.
+        self.parents = parents
+        #  Cpt è un dizionario che associa a una variabile (chiave) la sua tabella cpt.
+        self.cpt = cpt
+        # Variables è un dizionario che associa a una variabile(chiave) un valore intero
+        self.variables = {}
+
+        for i in range(len(self.nodes)):
+            self.variables[nodes[i]] = i
+
+    def joint_probability(self):
+        joint = np.zeros(2 ** len(self.nodes) + 1, len(self.nodes) + 1)
+        for j in range(len(self.nodes)):
+            joint[0][j] = self.nodes[j]
+        for k in range(2 ** len(self.nodes) + 1):
+            quoziente = k
+            # parto dal termine della matrice si prende l'indice della riga e si fa modulo 2 .
+            for t in range(len(self.nodes), -1, -1):
+                joint[k + 1][t] = quoziente % 2
+                quoziente = math.floor(quoziente / 2)
+
+        # Manca fare il join tra le cpt iniziali
+
+
+class JunctionTree:
+    def __init__(self, clusters, separators, egdes, beliefNetwork):
+        # Clusters è una lista di stringhe ognuna rappresenta un gruppo di variabili originali "ABC"
+        self.clusters = clusters
+        self.separators = separators
+        self.beliefNetwork = beliefNetwork
+        # CPTs e CPTc sono due dizionari che associano rispettivamente la stringa corrispondente al separatore/cluster
+        # a una lista , la sua cpt
+        self.CPTs = {}
+        self.CPTc = {}
+        # Edges è la liste degli archi del Jt , questi sono delle triplette (cluster,separatore,cluster)
+        self.edges = egdes
+        for i in range(len(self.clusters)):
+            self.CPTc[self.clusters[i]] = np.zeros(2 ** len(self.clusters[i]) + 1, len(self.clusters[i]) + 1)
+            for j in range(len(self.clusters[i])):
+                self.CPTc[self.clusters[i]][0][j] = beliefNetwork.variables[self.clusters[i][j]]
+            for k in range(2 ** len(self.clusters[i])):
+                quoziente = k
+                # parto dal termine della matrice si prende l'indice della riga e si fa modulo 2 .
+                for t in range(len(self.clusters[i]), -1, -1):
+                    self.CPTc[self.clusters[i]][k + 1][t] = quoziente % 2
+                    quoziente = math.floor(quoziente / 2)
+
+        for i in range(len(self.separators)):
+            self.CPTs[self.separators[i]] = np.zeros(2 ** len(self.separators[i]) + 1, len(self.separators[i]) + 1)
+            for j in range(len(self.separators[i])):
+                self.CPTs[self.separators[i]][0][j] = beliefNetwork.variables[self.separators[i][j]]
+            for k in range(2 ** len(self.separators[i])):
+                quoziente = k
+                for t in range(len(self.separators[i]), -1, -1):
+                    self.CPTs[self.separators[i]][k + 1][t] = quoziente % 2
+                    quoziente = math.floor(quoziente / 2)
+
+    # Questo è il metodo che definisce la propagazione dei messaggi.
+    def inizialization(self):
+        # inizializzo le cpt dei cluster e separatori a tutti elementi a 1
+        for i in range(len(self.clusters)):
+            self.CPTc[self.clusters[i]][:] = 1
+        for j in range(len(self.separators)):
+            self.CPTs[self.separators][j][:] = 1
+
+        found = True
+
+        for i in range(len(self.beliefNetwork.variables)):
+            for j in range(len(self.clusters)):
+                if self.clusters[j].find(self.beliefNetwork.nodes[i]) != -1:
+                    for k in range(len(self.beliefNetwork.parents[self.beliefNetwork.nodes[i]])):
+                        if self.clusters[j].find(self.beliefNetwork.parents[self.beliefNetwork.nodes[i]][k]) == -1:
+                            found = False
+                    if found == True:
+                        self.product(self, self.CPTc[self.clusters[j]],
+                                     self.beliefNetwork.cpt[self.beliefNetwork.nodes[i]])
+                        break
+                    else:
+                        found = True
+
+    def product(self, tv, ts):
+        # Trovo le colonne delle variabili in comune tra i due cluster
+        # list è una variabile che contiene gli indici delle colonne delle variabili in comune
+        list = []
+        # found è un var booleana che mi dice se tutta la riga della prima tabella ha negli
+        # attributi in comune gli stessi valori di una delle tuple della seconda tabella
+        found = True
+        for i in range(tv.shape[1] - 1):
+            for j in range(ts.shape[1] - 1):
+                if tv[0][i] == ts[0][j]:
+                    list.append((i, j))
+
+        for k in range(tv.shape[0] - 1):
+            for y in range(ts.shape[0] - 1):
+                for t in range(len(list)):
+                    if tv[k + 1][list[t][0]] != ts[y + 1][list[t][1]]:
+                        found = False
+                if found == True:
+                    tv[k + 1][tv.shape[1] - 1] = tv[k + 1][tv.shape[1] - 1] * ts[y + 1][ts.shape[1] - 1]
+                else:
+                    found = True
+
+        def division(self, tv, ts):
+            list = []
+            # found è un var booleana che mi dice se tutta la riga della prima tabella ha negli
+            # attributi in comune gli stessi valori di una delle tuple della seconda tabella
+            found = True
+            for i in range(tv.shape[1] - 1):
+                for j in range(ts.shape[1] - 1):
+                    if tv[0][i] == ts[0][j]:
+                        list.append((i, j))
+
+            for k in range(tv.shape[0] - 1):
+                for y in range(ts.shape[0] - 1):
+                    for t in range(len(list)):
+                        if tv[k + 1][list[t][0]] != ts[y + 1][list[t][1]]:
+                            found = False
+                    if found == True:
+                        if (tv[k + 1][tv.shape[1] - 1] / ts[y + 1][ts.shape[1] - 1]) != 0:
+                            tv[k + 1][tv.shape[1] - 1] = tv[k + 1][tv.shape[1] - 1] / ts[y + 1][ts.shape[1] - 1]
+                    else:
+                        found = True
+
+        def absorption(self, tv, ts, tw):
+            # uso il metodo copy di Numpy per creare una nuova tabella con i valori di ts
+            ts_new = ts.copy()
+            list = []
+            found = True
+            check = True
+            for i in range(tv.shape[1] - 1):
+                for j in range(ts.shape[1] - 1):
+                    if tv[0][i] == ts[0][j]:
+                        list.append((i, j))
+            # utilizzo la funzione Union Find per creare dei gruppi nelle righe di tv
+            # all'inizio sets contiene tanti gruppi quanti sono le righe di UF
+            sets = uf.UF(tv.shape[0] - 1)
+            while sets.count() > ts.shape[0] - 1:
+                # itero prendendo una riga della tabella e tutte le successive sulle variabili(colonne) in comune
+                for i in range(len(tv.shape[0]) - 2):
+                    for j in range(i + 2, len(tv.shape[0])):
+                        for k in range(len(list)):
+                            if tv[i + 1][list[k][0]] != tv[j][list[k][0]]:
+                                found = False
+                        # nel caso in cui ho trovato un riga valori uguali negli attributi
+                        # in comune con ts, verifico se non fa parte già del set con la funzione
+                        # connected
+                        if found == True and not sets.connected(i, j):
+                            sets.union(i, j)
+                            for y in range(len(ts.shape[0]) - 1):
+                                for x in range(len(list)):
+                                    if tv[i + 1][list[x][0]] != ts[y + 1][list[x][1]]:
+                                        check = False
+                                if check == True:
+                                    ts[y][ts.shape[1] - 1] += (tv[i + 1][tv.shape[1] - 1] + tv[j][tv.shape[1] - 1])
+                                else:
+                                    check = False
+                        else:
+                            found = False
+
+            self.division(self, ts, ts_new)
+            self.product(self, tw, ts)
+
+        def distribute_evidence(self, root):
+            # ricevo un nodo arbitrario,lo considero come la radice dell'albero
+            for i in range(len(self.egdes)):
+                if root in self.egdes[i]:
+                    if self.egdes[0] == root:
+                        absorption(self, self.CPTc[root], self.CPTs[self.egdes[1]], self.CPTc[self.egdes[2]])
+                        distribute_evidence(self, self.egdes[2])
+                    else:
+                        absorption(self, self.CPTc[self.egdes[0]], self.CPTs[self.egdes[1]], root)
+                        distribute_evidence(self, self.egdes[0])
+
+        def find_leaf(self, root):
+            list = []
+            for i in range(len(self.egdes)):
+                if root in self.egdes[i]:
+                    if self.egdes[0] == root:
+                        find_leaf(self, self.egdes[2])
+                    else:
+                        find_leaf(self, self.egdes[0])
+
+        #  Fix this
+
+        def collect_evidence(self, root):
+    # chiama la distribute una volta che dalle foglie è ritornato alla root
+
+    #  Fix me
---
+++ main.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import Inference as I
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Memorizzo la rete del file .bn , abrrevio il nome dei nodi alla iniziale
+
+bde = I.BeliefNetwork(["B", "E", "A", "J", "M"], [("B", []), ("E", []), ("A", [("E", "B")]), ("J", ["A"]),
+                                                  ("M", ["A"])], [("B", [0.99, 0.01]), ("E", [0.98, 0.02]),
+                                                                  ("A",
+                                                                   [0.999, 0.001, 0.71, 0.29, 0.06, 0.94, 0.05, 0.95]),
+                                                                  ("J", [0.95, 0.05, 0.1, 0.9]),
+                                                                  ("M", [0.99, 0.01, 0.3, 0.7])])
+
+# Memorizzo la rete simple.bn
+
+simple = I.BeliefNetwork(["A", "B", "C"], [("A", []), ("B", "A"), ("C", [])],
+                         [("A", [0.3, 0.7]), ("B", [0.4, 0.6, 0.1, 0.9]), ("C", [0.4, 0.6, 0.1, 0.9])])
+
+# Memorizzo il Junction Tree del file bde_test.bn , https://github.com/francescobaio/Elaborato_AI/blob/main/JunctionTree%20EarthQuake.bn.png
+
+
+Jt1 = I.JunctionTree(["AJ", "AM", "ABE"], ["A"], [("AJ", "A", "AM"), ("ABE", "A", "AJ"), ("ABE", "A", "AM")], bde)
+
+# Memorizzo il Junction Tree del file simple_bn , https://github.com/francescobaio/Elaborato_AI/blob/main/JunctionTree%20simple.bn.png
+
+
+Jt2 = I.JunctionTree(["AB", "BC"], ["B"], ["AB", "B", "BC"], simple)
+
+# Esempi relativi all'uso della definizione di probabilità condizionale
+
+# Calcolo della probabilità congiuntà tramite la def. di questa per una Belief NetWork
